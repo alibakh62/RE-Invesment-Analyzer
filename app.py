bb6926ab9d84c113143635030df8a69c57193222
Ali Bakhtiari <alibakh62@gmail.com>
2022-05-08 14:08:10 -0500

    made main menu more beautiful

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import pages.investment_criteria_deprecated
 import pages.investment_criteria
 import pages.search
 import pages.search_deprecated
+from streamlit_option_menu import option_menu
 
 import streamlit as st
 
@@ -13,17 +14,30 @@ st.set_page_config(layout="wide", page_title="Real Estate Investment Analyzer", 
 PAGES = {
     # "Default Assumptions": pages.default_assumptions.app,
     "Investment Criteria": pages.investment_criteria.app,
-    "Investment Criteria_deprecated": pages.investment_criteria_deprecated.app,
+    # "Investment Criteria_deprecated": pages.investment_criteria_deprecated.app,
     # "Review Assumptions": pages.investment_criteria_tmp.app,
     "Search": pages.search.app,
-    "Search_deprecated": pages.search_deprecated.app,
+    # "Search_deprecated": pages.search_deprecated.app,
     "Property Details": pages.property_details.app,
     # "Property Investment Worksheet": pages.search.app,
     "Scenario Analysis": pages.analysis.app,
     "Comparative Analysis": pages.analysis.app,
 }
 
-st.sidebar.title("Menu")
-selection = st.sidebar.selectbox("Pages", list(PAGES.keys()))
-page = PAGES[selection]
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Investoria",
+        options=list(PAGES.keys()),
+        icons=["sliders", "search", "house", "graph-up-arrow", "bar-chart"],
+        default_index=1,
+    )
+
+page = PAGES[selected]
 page()
+
+
+
+# st.sidebar.title("Menu")
+# selection = st.sidebar.selectbox("Pages", list(PAGES.keys()))
+# page = PAGES[selection]
+# page()
