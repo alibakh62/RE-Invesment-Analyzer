3e6ee505f30c6dd115c638e15bf031cc4d80fc1d
Ali Bakhtiari <alibakh62@gmail.com>
2022-05-09 23:37:43 -0500

    added general password for the app

--- app.py
+++ app.py
@@ -13,31 +13,61 @@ import streamlit as st
 
 st.set_page_config(layout="wide", page_title="Real Estate Investment Analyzer", initial_sidebar_state="expanded")
 
-PAGES = {
-    # "Default Assumptions": pages.default_assumptions.app,
-    "Investment Criteria": pages.investment_criteria.app,
-    # "Investment Criteria_deprecated": pages.investment_criteria_deprecated.app,
-    # "Review Assumptions": pages.investment_criteria_tmp.app,
-    "Search": pages.search.app,
-    # "Search_deprecated": pages.search_deprecated.app,
-    "Property Details": pages.property_details.app,
-    # "Property Investment Worksheet": pages.search.app,
-    "Scenario Analysis": pages.analysis.app,
-    "Scenario Analysis 1+": pages.analysis_multiple.app,
-    # "Scenario Analysis-deprecated": pages.analysis_deprecated.app,
-    "Comparative Analysis": pages.analysis.app,
-}
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
-with st.sidebar:
-    selected = option_menu(
-        menu_title="Investoria",
-        options=list(PAGES.keys()),
-        icons=["sliders", "search", "house", "graph-up-arrow", "bar-chart"],
-        default_index=1,
-    )
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
 
-page = PAGES[selected]
-page()
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+if check_password():
+
+    PAGES = {
+        # "Default Assumptions": pages.default_assumptions.app,
+        "Investment Criteria": pages.investment_criteria.app,
+        # "Investment Criteria_deprecated": pages.investment_criteria_deprecated.app,
+        # "Review Assumptions": pages.investment_criteria_tmp.app,
+        "Search": pages.search.app,
+        # "Search_deprecated": pages.search_deprecated.app,
+        "Property Details": pages.property_details.app,
+        # "Property Investment Worksheet": pages.search.app,
+        "Scenario Analysis": pages.analysis.app,
+        "Scenario Analysis 1+": pages.analysis_multiple.app,
+        # "Scenario Analysis-deprecated": pages.analysis_deprecated.app,
+        "Comparative Analysis": pages.analysis.app,
+    }
+
+    with st.sidebar:
+        selected = option_menu(
+            menu_title="Investoria",
+            options=list(PAGES.keys()),
+            icons=["sliders", "search", "house", "graph-up-arrow", "bar-chart"],
+            default_index=1,
+        )
+
+    page = PAGES[selected]
+    page()
 
 
 
